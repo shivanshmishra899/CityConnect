d9fb93ebc5ba1537ed9fbbc6ed29c71fb035e041
Shivansh Mishra <yourgithubemail@example.com>
2025-09-15 09:44:22 +0530

    index.js corrected

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-// This is a simple component that will be displayed on the page
-function App() {
-  // You can replace this with the HTML/JS from our previous index.html file later
-  return <h1>Hello from React! Your CityConnect app is running.</h1>;
-}
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    import App from './App.jsx'; // Make sure this imports from your .jsx file
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+    
